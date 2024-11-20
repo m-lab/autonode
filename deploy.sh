5a10aee30a8fe6eae523857803bbd27a9e38c44e
Stephen Soltesz <stephen.soltesz@gmail.com>
2024-11-20 13:44:53 -0500

    Add whoami

--- deploy.sh
+++ deploy.sh
@@ -31,19 +31,22 @@ if test -f ${DOCKER_COMPOSE_FILE_PATH}; then
 fi
 
 # Copy the docker compose file to the VM.
-gcloud --project ${PROJECT} compute scp --zone ${VM_ZONE} ${DOCKER_COMPOSE_FILE_PATH} ${VM_NAME}:~/docker-compose.yml --tunnel-through-iap
+gcloud --project ${PROJECT} compute scp --zone ${VM_ZONE} ${DOCKER_COMPOSE_FILE_PATH} autonode@${VM_NAME}:~/docker-compose.yml --tunnel-through-iap
 
 # TODO(soltesz): remove root@ logic after deployment.
 # Shutdown the version running as root.
-gcloud --project ${PROJECT} compute ssh --quiet --zone ${VM_ZONE} root@${VM_NAME} --tunnel-through-iap <<EOF
+gcloud --project ${PROJECT} compute ssh --zone ${VM_ZONE} root@${VM_NAME} --tunnel-through-iap <<EOF
+    set -x
+    whoami
     # Stop the docker compose if it's running.
     docker compose -f docker-compose.yml down
 EOF
 
 # Setup script. This stops docker compose, creates the required folders,
 # creates the .env file and restarts docker compose.
-gcloud --project ${PROJECT} compute ssh --quiet --zone ${VM_ZONE} autonode@${VM_NAME} --tunnel-through-iap <<EOF
+gcloud --project ${PROJECT} compute ssh --zone ${VM_ZONE} autonode@${VM_NAME} --tunnel-through-iap <<EOF
     set -euxo pipefail
+    whoami
     # Create volume folders if not present.
     mkdir -p autocert autonode certs html schemas resultsdir
 
